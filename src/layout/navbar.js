3fc2ba59e8e8e8aa644687fab835869a24f6b78e
Mampenda <amalie.mampenda@gmail.com>
2024-09-06 19:55:41 +0200

    Started connecting frontend and backend

--- src/layout/navbar.js
+++ src/layout/navbar.js
@@ -19,6 +19,8 @@ export default function Navbar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
+          {/* <button className="btn btn-outline-light">Create Poll</button>
+          <button className="btn btn-outline-light">Log In</button> */}
           <button className="btn btn-outline-light">Add User</button>
         </div>
       </nav>
---
+++ src/pages/home.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Home() {
+  //Object for storing user information (initialized as empty array)
+  const [users, setUsers] = useState([]);
+
+  // Runs once only when the page loads (If we don't use empty array as arg, this will load infinitely)
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  //Load users using async/await (since java is executing line by line)
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:8080/users");
+    setUsers(result.data);
+
+    //Show data from (backend) database in console
+    // console.log(result.data);
+  };
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        <table className="table border shadow">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Username</th>
+              <th scope="col">E-mail</th>
+              <th scope="col">Action</th>
+              {/* <th scope="col">Password</th>
+              <th scope="col">Polls</th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {/*Get dynamic data from database (create new array from function-call on every array element in db)*/}
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                {/* <td>{user.password}</td>
+                <td>{user.polls}</td> */}
+                <td>
+                  <button className="btn btn-primary mx-2">View</button>
+                  <button className="btn btn-outline-primary mx-2">Edit</button>
+                  <button className="btn btn-danger mx-2">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
